8ccc763cd90c227e3b173615ae260aa34cd5a94a
ronaldo <ronaldomalta9@gmail.com>
2026-03-09 11:35:40 -0300

    atividades de casa1

---
+++ Variáveis e Expressões/questao1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main{
+float real, cotacao;
+printf("qual e o valor que vc quer colocar: \n", real);
+scanf("2.%f", real);
+cotacao= real * 5.222;
+printf("vc tem %f dolares \n", cotacao)
+return 0;
+}
