567cac413a8d376a327f52cd883948b2a0ace669
ronaldo <ronaldomalta9@gmail.com>
2026-03-16 09:07:49 -0300

    atividade de sala 16/03

---
+++ atividade de aula/vetor5posi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int vetor[5];
+    int contador = 0;
+ 
+    for(int i=0; i<5; i++){
+        printf("digite um numero: ");
+        scanf("%d", &vetor[i]);
+    }
+    for(i=0;i<5; i++){
+        if (vetor[i]<10);
+        contador ++;
+    }
+
+    printf("a quantidade de numeros maiores que 10 sao: %d \n", contador);
+
+
+
+    return 0;
+}
