563cb23adf6e126685ac17414efaf4e3965147c6
ronaldo <ronaldomalta9@gmail.com>
2026-03-19 08:53:05 -0300

    atualizaçao 19/03

---
+++ atividade de aula/vetor5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int n= 5;
+    int v[n];
+
+for(int i=0;i<n;i++){
+    scanf("%d", &v[i]);
+
+}
+int valor, encontrado=0;
+scanf("%d", %valor);
+    if (valor==v[i])[
+        encontrado==1;
+    ]
+
+    if (encontrado==1){
+        printf("valor foi encontrado\n");
+    }
+    else{
+        printf("o valor nao foi enontrado\n");
+    }
+
+    return 0;
+}
---
+++ atividade de aula/vetor6.c
@@ -0,0 +1,22 @@
+adw#include<stdio.h>
+
+int main(){
+    int v[n]
+    int n= 7;
+    // leitura d vetor
+    for(int i; i<7; i++){
+    scanf("%d", &v[i]);
+    }
+
+    int maior = v[0];
+    int posiçao = 0;
+
+    for(i=0; i<7; i++){
+        if (v[i]> maior);
+            maior=v[i];
+            posicao=i;
+    }
+    printf("maior valor encontrado foi %d",maior);
+    printf("sua posicao foi %d",posicao)
+    return 0;
+}
