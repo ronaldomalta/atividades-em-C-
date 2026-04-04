762e5258b2e5d8ee8fbd7ac68f9f72b644cfbcbd
ronaldo <ronaldomalta9@gmail.com>
2026-04-03 21:56:03 -0300

    funçoes

---
+++ aaa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int X[i];
+    for (int i = 0; i < 10; i++){
+        scanf("%d", &x[i]);
+
+    }
+    if(x[10] == 0 || x[10] < 0){
+        x[10] = 1;
+    }
+    printf("x[%d] = %d", i, x[10])
+
+    return 0;
+}
--- atividade de aula/vetor6.c
+++ atividade de aula/vetor6.c
@@ -1,4 +1,4 @@
-adw#include<stdio.h>
+#include<stdio.h>
 
 int main(){
     int v[n]
